bf559dedf954eb075b8d54fbd412e932355637cd
Pande <popovski.pande@gmail.com>
2022-11-28 11:33:23 +0100

    Define page style and page strusture
    Decided what style the webpage would have, as well as the structure.
    Next I need to start working with the DOM manipulation to make the
    app functional.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+The goal of this project is to create a web To-Do List app where you can 
+add any number of projects that contain a title, description and a checklist
+that can be modified by the user as well.
+
+Background photo: https://www.pexels.com/@enginakyurt/
+
 # todo-list
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+
+const toDoList = [
+  {
+    title: "To-Do List",
+    description: "Create a functioning To-Do List app that works in the browser. Better get busy.",
+    checkList: ["Create basic page setup", "Define page style and structure",
+  ""]
+  }
+]
