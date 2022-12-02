ac119301ffaa7af5231132b155a9569c51749adb
Pande <popovski.pande@gmail.com>
2022-12-02 13:01:03 +0100

    Work on displaying project on screen
    Added code that displays the rest of the content for the project
    description. Still need to mae some adjustments to it however.
    
    Also added a function that should remove the displayed project,
    and display the selected project from the sidebar. I still need to
    couple this one with an event listener which I plan to add to the
    sidebar buttons..

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//need to:
+//add functionality for the color of the sidebar borders depending on project priority
+//add checkboxes for the list items in the project checklist
+
 import './style.css';
 
 const toDoList = [
@@ -6,12 +10,17 @@ const toDoList = [
     description: "Create a functioning To-Do List app that works in the browser. Better get busy.",
     checkList: ["Create basic page setup", "Define page style and structure",
   ""],
+    dueDate: "12/10/2022",
+    priority: "High",
   },
   {
     title: "Work Out",
     description: "Go to the gym and work out.",
     checkList: ["Go to gym", "Work out", "Go back"],
+    dueDate: "12/03/2022",
+    priority: "Medium",
   },
+  
 ]
 
 //create sidevar div where projects will be listed
@@ -52,24 +61,70 @@ const sideBarContent = () => {
 }
 
 //generate project details (takes data-index as input, need to couple this with an event listener)
+//might be a good idea to call a method for generating the elements here?
 const mainAreaContent = (dataIndex) => {
+  //main container
   const mainContainer = document.querySelector('.main');
   const projectContainer = document.createElement('div');
   projectContainer.classList.add('project-container');
   mainContainer.appendChild(projectContainer);
 
+  //title
   const projectTitle = document.createElement('h1');
   projectTitle.classList.add('title');
   projectTitle.textContent = toDoList[dataIndex].title;
   projectContainer.appendChild(projectTitle);
 
+  //description
   const projectDescription = document.createElement('p');
   projectDescription.classList.add('text');
   projectDescription.textContent = toDoList[dataIndex].description;
   projectContainer.appendChild(projectDescription);
 
+  //due date
+  const dueDate = document.createElement('div');
+  dueDate.classList.add('due-date');
+  dueDate.textContent = toDoList[dataIndex].dueDate;
+  projectContainer.appendChild(dueDate);
+
+  //priority
+  const priority = document.createElement('div');
+  priority.classList.add('priority');
+  priority.textContent = toDoList[dataIndex].priority;
+  projectContainer.appendChild(priority);
+
+  //checklist
+  const checkList = document.createElement('div');
+  checkList.classList.add('checklist');
+  projectContainer.appendChild(checkList);
+
+  const addItem = document.createElement('button');
+  addItem.classList.add('add-item');
+  addItem.textContent = "Add item to Checklist";
+  checkList.appendChild(addItem);
+
+  //need to add checkboxes for each item here
+  const unorderedList = document.createElement('ul');
+  unorderedList.classList.add('all-items')
+  checkList.appendChild(unorderedList);
+  for (let i = 0; i < toDoList[dataIndex].checkList.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+    listItem.textContent = toDoList[dataIndex].checkList[i];
+    unorderedList.appendChild(listItem);
+  }
+
   /* NEED TO FINISH THIS */
 }
+
+/* clear the screen of the displayed project, and display the one that 
+  was selected NEED TO COUPLE THIS WITH AN EVENT LISTENER*/
+  const generateMainContent = (dataIndex) => {
+    const mainContainer = document.querySelector('project-container');
+    mainContainer.remove();
+
+    mainAreaContent(dataIndex);
+  }
 
 
 
